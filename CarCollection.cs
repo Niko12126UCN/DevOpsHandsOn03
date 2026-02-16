23fa03e48b74efc4c6a615664e2265af305f5743
Niko12126UCN <150657998+Niko12126UCN@users.noreply.github.com>
2026-02-16 10:00:50 +0100

    adding tests

---
+++ CarCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsHandsOn03
+{
+    public class CarCollection
+    {
+        private List<string> cars;
+        public CarCollection()
+        {
+            cars = new List<string>();
+        }
+        public void AddCar(string car)
+        {
+            if (!string.IsNullOrEmpty(car))
+            {
+                cars.Add(car);
+            }
+        }
+        public void RemoveCar(string car)
+        {
+            if (cars.Contains(car))
+            {
+                cars.Remove(car);
+            }
+        }
+        public List<string> GetCars()
+        {
+            return new List<string>(cars);
+        }
+    }
+}
