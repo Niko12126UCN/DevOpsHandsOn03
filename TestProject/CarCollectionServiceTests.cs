1ec45d35f9d4f725d9302f1eee138f3e65ef18b4
Niko12126UCN <150657998+Niko12126UCN@users.noreply.github.com>
2026-02-16 10:22:26 +0100

    init

---
+++ TestProject/CarCollectionServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public class CarCollectionServiceTests
+    {
+        [Fact]
+        public void AddRandomCar_ShouldAddSpecifiedAmountOfCars()
+        {
+            // Arrange
+            var carCollectionService = new DevOpsHandsOn03.CarCollectionService();
+            int amount = 5;
+            // Act
+            carCollectionService.AddRandomCar(amount);
+            var cars = carCollectionService.GetCars();
+            // Assert
+            Assert.Equal(amount, 3);
+        }
+    }
+}
---
+++ TestProject/CarCollectionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace TestProject
+{
+    public class CarCollectionTests
+    {
+        [Fact]
+        public void AddCar_ShouldAddCarToCollection()
+        {
+            // Arrange
+            var carCollection = new DevOpsHandsOn03.CarCollection();
+            string car = "Toyota";
+            // Act
+            carCollection.AddCar(car);
+            var cars = carCollection.GetCars();
+            // Assert
+            Assert.Contains(car, cars);
+        }
+
+        [Fact]
+        public void RemoveCar_ShouldRemoveCarFromCollection()
+        {
+            // Arrange
+            var carCollection = new DevOpsHandsOn03.CarCollection();
+            string car = "Honda";
+            carCollection.AddCar(car);
+            // Act
+            carCollection.RemoveCar(car);
+            var cars = carCollection.GetCars();
+            // Assert
+            Assert.DoesNotContain(car, cars);
+        }
+    }
+}
