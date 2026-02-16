10508213f0bdaf1b9bbad4e680637bd26d439904
Niko12126UCN <150657998+Niko12126UCN@users.noreply.github.com>
2026-02-16 10:13:58 +0100

    added service

---
+++ CarCollectionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsHandsOn03
+{
+    public class CarCollectionService
+    {
+        private CarCollection carCollection;
+        public CarCollectionService()
+        {
+            carCollection = new CarCollection();
+        }
+
+        public void AddRandomCar(int amount)
+        {
+            var random = new Random();
+            var carBrands = new List<string> { "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Mercedes" };
+
+            for (int i = 0; i < amount; i++)
+            {
+                string randomCar = carBrands[random.Next(carBrands.Count)];
+                carCollection.AddCar(randomCar);
+            }
+        }
+
+        public List<string> GetCars()
+        {
+            return carCollection.GetCars();
+        }
+}
+}
